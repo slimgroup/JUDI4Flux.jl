65306674fab2d48524ba6b6a42b69af62d9f4a30
Francis <ziyi.yin.97@gmail.com>
2021-03-08 11:09:26 -0500

    add adjoint rules for Forward modeling

--- src/JUDI4Flux.jl
+++ src/JUDI4Flux.jl
@@ -34,11 +34,10 @@ module JUDI4Flux
     # Extended source forward modeling
     struct ExtendedQForward{T1}
         F::T1   # forward modeling operator
-        w_grad::Bool
         m_grad::Bool
     end
 
-    ExtendedQForward(F; w_grad=true, m_grad=true) = ExtendedQForward(F, w_grad, m_grad)
+    ExtendedQForward(F; m_grad=true) = ExtendedQForward(F, m_grad)
 
     # Extended source forward modeling: forward mode
     function (EQF::ExtendedQForward)(w::AbstractArray, m::AbstractArray)
@@ -67,17 +66,9 @@ module JUDI4Flux
 
     @adjoint function (EQF::ExtendedQForward)(w::AbstractArray, m::AbstractArray)
         if EQF.m_grad
-            if EQF.w_grad
-                return EQF(w, m), Δ -> (nothing, grad_w(EQF, m, Δ), grad_m(EQF, w, m, Δ))
-            else
-                return EQF(w, m), Δ -> (nothing, nothing, grad_m(EQF, w, m, Δ))
-            end
+            return EQF(w, m), Δ -> (nothing, grad_w(EQF, m, Δ), grad_m(EQF, w, m, Δ))
         else
-            if EQF.w_grad
-                return EQF(w, m), Δ -> (nothing, grad_w(EQF, m, Δ), nothing)
-            else
-                return EQF(w, m), Δ -> (nothing, nothing, nothing)
-            end
+            return EQF(w, m), Δ -> (nothing, grad_w(EQF, m, Δ), nothing)
         end
     end
 
@@ -87,11 +78,10 @@ module JUDI4Flux
     # Extended source adjoint modeling
     struct ExtendedQAdjoint{T1}
         F::T1   # adjoint modeling operator
-        d_grad::Bool
         m_grad::Bool
     end
 
-    ExtendedQAdjoint(F; d_grad=true, m_grad=true) = ExtendedQAdjoint(F, d_grad, m_grad)
+    ExtendedQAdjoint(F; m_grad=true) = ExtendedQAdjoint(F, m_grad)
 
     # Extended source adjoint modeling: forward mode
     function (EQT::ExtendedQAdjoint)(d::AbstractArray, m::AbstractArray)
@@ -121,17 +111,31 @@ module JUDI4Flux
 
     @adjoint function (EQT::ExtendedQAdjoint)(d::AbstractArray, m::AbstractArray)
         if EQT.m_grad
-            if EQT.d_grad
-                return EQT(d, m), Δ -> (nothing, grad_d(EQT, m, Δ), grad_m(EQT, d, m, Δ))
-            else
-                return EQT(d, m), Δ -> (nothing, nothing, grad_m(EQT, d, m, Δ))
-            end
+            return EQT(d, m), Δ -> (nothing, grad_d(EQT, m, Δ), grad_m(EQT, d, m, Δ))
         else
-            if EQT.d_grad
-                return EQT(d, m), Δ -> (nothing, grad_d(EQT, m, Δ), nothing)
-            else
-                return EQT(d, m), Δ -> (nothing, nothing, nothing)
-            end
+            return EQT(d, m), Δ -> (nothing, grad_d(EQT, m, Δ), nothing)
         end
     end
+
+    # Fixed source forward modeling
+    struct Forward{T1}
+        F::T1   # forward modeling operator
+        q::AbstractArray
+    end
+
+    # Fixed source forward modeling: forward mode
+    function (FWD::Forward)(m::AbstractArray)
+        Flocal = deepcopy(FWD.F)
+        Flocal.model.m .= m[:, :, 1,1]
+        out = Flocal * vec(Forward.q)
+        nt = Flocal.recGeometry.nt[1]
+        nrec = length(Flocal.recGeometry.xloc[1])
+        return reshape(out, nt, nrec, 1, Flocal.info.nsrc)
+    end
+
+    @adjoint function (FWD::Forward)(m::AbstractArray)
+        J = judiJacobian(FWD.F,FWD.q)
+        return FWD(m), Δ -> (nothing, transpose(J) * Δ)
+    end
+
 end
